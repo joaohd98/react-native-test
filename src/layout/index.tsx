b0e3c88de053240884ad1d8b1d5c0d23c43d166d
João Damazio <joaohd98@gmail.com>
2020-06-10 13:09:55 -0300

    Add test

---
+++ src/layout/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+export const Layout = () => (
+  <>
+    <StatusBar barStyle="dark-content" />
+    <View>
+      <Text>
+        ABC
+      </Text>
+    </View>
+  </>
+);
+
